ae4883c127c23b8ad57c407169682ac89ef9d389
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-23 09:48:33 +0530

    ok

---
+++ MultipleRide/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CalculateFare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase1 obj = new UseCase1();
+            int totalFare = 0, rideFare = 0,ch;
+            Console.WriteLine("Total Fare : " + totalFare);
+            do
+            {
+                Console.WriteLine("Do You Want To Take Ride With Us ? : 1");
+                Console.WriteLine("Work Done Exit : 2");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("Enter your choice");
+                ch= int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        rideFare = obj.invoiceGenerator();
+                        totalFare = totalFare + rideFare;
+                        Console.WriteLine("Ride Fare : " + rideFare);
+                        Console.WriteLine("-----------");
+                        break;
+                    case 2:
+                        Console.WriteLine("Thanks for taking ride with us, Have a nice day");
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid choice");
+                        break;
+
+                }
+            } while (ch != 2);
+            Console.WriteLine("----------------");
+            Console.WriteLine("Total Fare : " + totalFare);
+        }
+    }
+}
---
+++ MultipleRide/UseCase1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateFare
+{
+    public class UseCase1
+    {
+        public int invoiceGenerator()
+        {
+            int farePerKM = 10;
+            Console.WriteLine("Enter Distance");
+            int distance = int.Parse(Console.ReadLine());
+            if(distance<=0)
+            {
+                Console.WriteLine("Enter distance greater than 0");
+                invoiceGenerator();
+            }
+            return farePerKM * distance;
+
+        }
+    }
+}
