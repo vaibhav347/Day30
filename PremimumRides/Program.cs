656db65dbd896aa9737166d8d7f89f6c07b7a330
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-23 09:50:01 +0530

    ok

---
+++ PremimumRides/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CalculateFare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase1 obj = new UseCase1();
+            float totalFare=0, rideFare=0,avgFarePerRide=0,premiumFare=15,normalFare=10;
+            int totalRides = 0, ch;
+            do
+            {
+                Console.WriteLine("Premimum Ride : 1");
+                Console.WriteLine("Normal Ride: 2");
+                Console.WriteLine("Work Done Exit : 3");
+                Console.WriteLine("-----------------");
+                Console.WriteLine("Enter your choice");
+                ch= int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        totalRides++;
+                        rideFare = obj.invoiceGenerator(premiumFare);
+                        totalFare = totalFare + rideFare;
+                        Console.WriteLine("Ride Fare : " + rideFare);
+                        Console.WriteLine("-----------");
+                        break;
+                    case 2:
+                        totalRides++;
+                        rideFare = obj.invoiceGenerator(normalFare);
+                        totalFare = totalFare + rideFare;
+                        Console.WriteLine("Ride Fare : " + rideFare);
+                        Console.WriteLine("-----------");
+                        break;
+                    case 3:
+                        Console.WriteLine("Thanks for taking ride with us, Have a nice day");
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid choice");
+                        break;
+
+                }
+            } while (ch != 3);
+            Console.WriteLine("----------------");
+            Console.WriteLine("Total Rides :" + totalRides);
+            Console.WriteLine("Total Fare : " + totalFare);
+            avgFarePerRide = totalFare / totalRides;
+            Console.WriteLine("Average Fare Per Ride : " + avgFarePerRide);
+        }
+    }
+}
---
+++ PremimumRides/UseCase1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculateFare
+{
+    public class UseCase1
+    {
+        public float invoiceGenerator(float farePerKM)
+        {
+            Console.WriteLine("Enter Distance");
+            float distance = float.Parse(Console.ReadLine());
+            if(distance<=0)
+            {
+                Console.WriteLine("Enter distance greater than 0");
+                invoiceGenerator(farePerKM);
+            }
+            if(farePerKM==10)
+            {
+                float normal=farePerKM* distance;
+                if (normal < 5)
+                {
+                    Console.WriteLine("Ride fare must be greater than Rs 5");
+                    invoiceGenerator(farePerKM);
+                }
+            }
+            if (farePerKM == 15)
+            {
+                float premium = farePerKM * distance;
+                if (premium < 20)
+                {
+                    Console.WriteLine("Ride fare must be greater than Rs 20");
+                    invoiceGenerator(farePerKM);
+                }
+            }
+            return farePerKM * distance;
+
+        }
+    }
+}
