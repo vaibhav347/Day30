1aa2c60245a20b4880f85fd089910ad4eff192f5
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-23 09:47:41 +0530

    ok

---
+++ CalculateFare/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CalculateFare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseCase1 obj = new UseCase1();
+            int totalFare=obj.invoiceGenerator();
+            Console.WriteLine("Total Fare : " + totalFare);
+        }
+    }
+}
